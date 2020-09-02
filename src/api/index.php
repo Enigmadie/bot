108907298aa200b203c532434d0ed376692a3008
Enigmadie <gagen666@mail.ru>
2020-09-02 22:26:43 +0300

    fix

--- src/api/index.php
+++ src/api/index.php
@@ -11,20 +11,20 @@ function vk_api_call($method, $params = array()) {
   $params['v'] = VK_API_VERSION;
   $query = http_build_query($params);
   $url = VK_API_ENDPOINT . $method . '?' . $query;
-  file_get_contents($url);
-  /* $curl = curl_init($url); */
-  /* curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); */
-  /* $json = curl_exec($curl); */
-  /* $error = curl_error($curl); */
-  /* if ($error) { */
-  /*   throw new Exception("Failed {$method} request"); */
-  /* } */
-  /* curl_close($curl); */
-  /* $response = json_decode($json, true); */
-  /* if (!$response || !isset($response['response'])) { */
-  /*   throw new Exception("Invalid response for {$method} request"); */
-  /* } */
-  /* return $response['response']; */
+  /* file_get_contents($url); */
+  $curl = curl_init($url);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  $json = curl_exec($curl);
+  $error = curl_error($curl);
+  if ($error) {
+    throw new Exception("Failed {$method} request");
+  }
+  curl_close($curl);
+  $response = json_decode($json, true);
+  if (!$response || !isset($response['response'])) {
+    throw new Exception("Invalid response for {$method} request");
+  }
+  return $response['response'];
 }
 
 function vk_api_msgSend($peer_id, $text) {
