d1a75b94ececdaa7ae8e800f43cc57d19adf2814
Enigmadie <gagen666@mail.ru>
2020-09-05 00:00:08 +0300

    add weather services

--- src/services/wether.php
+++ src/services/wether.php
@@ -44,9 +44,9 @@ function get_forecasts($region) {
   if (!$response) {
     throw new Exception("Invalid response for {$region} request");
   }
-  print_r($response);
+  echo(json_encode($response));
   $filtered_date = array_filter($response[0], fn($key) => $key % 3 === 0, ARRAY_FILTER_USE_KEY);
-  print_r($filtered_date);
+  echo(json_encode($filtered_date));
   return $filtered_date;
 }
 
