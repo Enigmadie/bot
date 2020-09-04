baad8c9ee4678307e70b7dc7e10e27563bbe2dc7
Enigmadie <gagen666@mail.ru>
2020-09-04 23:53:48 +0300

    add weather services

--- src/routes.php
+++ src/routes.php
@@ -13,29 +13,33 @@ $app->get('/', function(ServerRequest $req, Response $res) {
 $app->post('/bot', function(ServerRequest $req, Response $res) {
   $data = json_decode(file_get_contents('php://input'));
   include '.env.php';
-  switch ($data->type) {
-    case 'confirmation':
-      echo $CONF_TOKEN;
-      break;
-    case 'message_new':
-      $message_text = $data->object->message->text;
-      $chat_id = $data->object->message->from_id;
-      if ($message_text == "привет"){
-        vk_api_msgSend($chat_id, "Привет, я бот, который говорит две фразы.");
-      }
-      if ($message_text == "пока"){
-        vk_api_msgSend($chat_id, "Пока. Если захочешь с кем-то поговорить, то у тебя есть бот, который говорит две фразы.");
-      }
-      if (preg_match('/погода\s/', $message_text)) {
-        $region = strstr($message_text, ' ');
-        $city = substr($region, 1);
-        $weather = get_weather($city);
-        vk_api_msgSend($chat_id, $weather);
-      }
-      echo('ok');
-      break;
-    default:
-      echo('ok');
+  try {
+    switch ($data->type) {
+      case 'confirmation':
+        echo $CONF_TOKEN;
+        break;
+      case 'message_new':
+        $message_text = $data->object->message->text;
+        $chat_id = $data->object->message->from_id;
+        if ($message_text == "привет"){
+          vk_api_msgSend($chat_id, "Привет, я бот, который говорит две фразы.");
+        }
+        if ($message_text == "пока"){
+          vk_api_msgSend($chat_id, "Пока. Если захочешь с кем-то поговорить, то у тебя есть бот, который говорит две фразы.");
+        }
+        if (preg_match('/погода\s/', $message_text)) {
+          $region = strstr($message_text, ' ');
+          $city = substr($region, 1);
+          $weather = get_weather($city);
+          vk_api_msgSend($chat_id, $weather);
+        }
+        echo('ok');
+        break;
+      default:
+        echo('ok');
+    }
+  } catch(Exception $e) {
+    echo('ok');
   }
   return $res;
 });
