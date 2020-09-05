f4053ff9383380e8ba4d9db438114e6343896774
Enigmadie <gagen666@mail.ru>
2020-09-06 01:03:33 +0300

    add weather services

--- src/routes.php
+++ src/routes.php
@@ -8,17 +8,16 @@ require_once './src/selectors/message_selector.php';
 $app->post('/bot', function(ServerRequest $req, Response $res) {
   include '.env.php';
   $data = json_decode(file_get_contents('php://input'));
-  ['type' => $type, 'object' => $object] = $data;
   try {
-    switch ($type) {
+    switch ($data->type) {
       case 'confirmation':
         echo($CONF_TOKEN);
         break;
       case 'message_new':
-        $message_text = $object['message']['text'];
-        $chat_id = $object['message']['from_id'];
-        $formated_msg = lcfirst($message_text);
-        msg_selector($formated_msg, $chat_id);
+        $message_text = $data->object->message->text;
+        $chat_id = $data->object->message->from_id;
+        $formatedMsg = lcfirst($message_text);
+        msg_selector($formatedMsg, $chat_id);
         echo('ok');
         break;
       default:
